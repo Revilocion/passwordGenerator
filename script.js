59cd10403bce4fdd86fb13394fefd0776bd6ee8b
Marquis Oliver <marquis.w.oliver@gmail.com>
2020-02-08 16:49:03 -0800

    code that actually works

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var display = document.getElementById("container2");
 var password = document.getElementById("button");
 var holder = document.getElementById("place");
-console.log("hello");
-var gen = "";
+
+var userinput = [];
 
 var lowerCase = [
   "a",
@@ -32,9 +32,9 @@ var lowerCase = [
   "y",
   "z"
 ];
-var numbers = [("1", "2", "3", "4", "5", "6", "7", "8", "9")];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var upperCase = [
-  ("A",
+  "A",
   "B",
   "C",
   "D",
@@ -49,23 +49,38 @@ var upperCase = [
   "M",
   "N",
   "O",
-  "P")
+  "P"
 ];
-var special = [("!", "@", "#", "$", "%", "^", "&", "*", "(", ")")];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+//var passwordLengthInput = prompt("How many characters do you want your password to be?");
+//var passwordLength = 0; //Default length for password.
+
+if (passwordLengthInput !== null && passwordLengthInput !== "") {
+  passwordLength = parseInt(passwordLengthInput);
+} else {
+  //to do: tell user
+}
 
 password.addEventListener("click", function() {
   //This didn't work...
-  console.log("this worked");
   lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  myArray[Math.floor(Math.random() * numbers.length)];
+  numbers[Math.floor(Math.random() * numbers.length)];
   upperCase[Math.floor(Math.random() * upperCase.length)];
   special[Math.floor(Math.random() * special.length)];
 
   //how am I going to be able to tie this entire function into showing the password?
 
   //we may need the outcome of this function to equal that of the "holder"
-  console.log("Almost there");
-  gen.textContent = password;
+  var genpassword =
+    lowerCase[Math.floor(Math.random() * lowerCase.length)] +
+    numbers[Math.floor(Math.random() * numbers.length)] +
+    upperCase[Math.floor(Math.random() * upperCase.length)] +
+    special[Math.floor(Math.random() * special.length)];
+
+  console.log(genpassword);
+
+  holder.textContent = genpassword;
 });
 
 /*When the button is clicked it will run the function listed up top.
